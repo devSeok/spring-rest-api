63b9bd9faab2c5baeb3c6f67e1f671f6b68719a6
dq-jinseok <ohjs@dinnerqueen.net>
2021-06-15 10:58:28 +0900

    수정

--- src/main/java/restapi/demo/config/WebConfig.java
+++ src/main/java/restapi/demo/config/WebConfig.java
@@ -1,9 +1,9 @@
 package restapi.demo.config;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import restapi.demo.config.auth.LoginUserArgumentResolver;
 
@@ -11,12 +11,11 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
-
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolverList) {
-        argumentResolverList.add(loginUserArgumentResolver);
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
     }
 }
